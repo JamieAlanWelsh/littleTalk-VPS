08c5ce7d5885c6f15076fa919059674c0de07ae9
Jamie Welsh <jw.jamiewelsh@gmail.com>
2024-12-04 17:43:31 +0000

    fixing of page titles

--- littleTalkApp/urls.py
+++ littleTalkApp/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('game/<str:game_name>/', views.game_description, name='game_description'),
-    # path('colour-semantics/', views.colour_semantics, name='colour_semantics'),
+    path('exercise/<str:game_name>/', views.game_description, name='game_description'),
 ]
