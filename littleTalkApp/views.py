90c9796e89d580b29e039f0eb4c4ddd17a659d79
Jamie Welsh <jw.jamiewelsh@gmail.com>
2024-12-04 17:09:42 +0000

    Patch for video embed

--- littleTalkApp/views.py
+++ littleTalkApp/views.py
@@ -12,12 +12,14 @@ def game_description(request, game_name):
             'description': 'Colourful semantics is an approach designed to support children developing their understanding of sentence structure and expand the length of their sentences.',
             'description2': 'The structure of a sentence (syntax) is colour coded and then these are linked to their meaning (semantics)',
             'link': 'https://jamiealanwelsh.github.io/colour-semantics-webgl/',
+            'video_id': 'FcdA00Tj5N0',
         },
         'exercise2': {
             'title': 'Exercise 2',
             'description': 'Another fun learning activity.',
             'description2': '',
             'link': 'https://jamiealanwelsh.github.io/exercise2/',
+            'video_id': '',
         },
     }
     game = game_descriptions.get(game_name, None)
@@ -25,3 +27,7 @@ def game_description(request, game_name):
         return render(request, '404.html', status=404)  # Return a 404 if the game name is invalid
     
     return render(request, 'game_description.html', {'game': game})
+
+
+# def colour_semantics(request):
+#     return render(request, 'colour_semantics.html')
