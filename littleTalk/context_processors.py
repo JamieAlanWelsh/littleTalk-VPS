a9cb274213b7a92cc1ebf0598e955d5a8fcf242e
Jamie Welsh <jw.jamiewelsh@gmail.com>
2025-09-02 15:44:04 +0100

    fixed canaconical tags with context processor

--- littleTalk/context_processors.py
+++ littleTalk/context_processors.py
@@ -2,3 +2,7 @@ def layout_context(request):
     return {
         'hide_sidebar': getattr(request, 'hide_sidebar', False),
     }
+
+def canonical_url(request):
+    canonical = request.build_absolute_uri(request.path)
+    return {'canonical_url': canonical}
